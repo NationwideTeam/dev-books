3aaa45f10ae4d843427ecacbe95fe1fbe7cfbe59
Jason Park <stiger3820@gmail.com>
2022-07-05 00:17:31 +0900

    feat: 추가 button 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import Button from "./components/buttons/button";
+
 function App() {
   return (
     <div className="App">
-      
+      <div className="App">
+        <Button disabled>안녕</Button>
+        <Button size="sm">안녕</Button>
+        <Button size="ms">안녕</Button>
+        <Button size="md">안녕</Button>
+        <Button size="lg" disabled>
+          안녕
+        </Button>
+        <Button disabled>안녕</Button>
+      </div>
     </div>
   );
 }
---
+++ src/components/buttons/button.jsx
@@ -0,0 +1,67 @@
+import styled, { css } from "styled-components";
+
+//버튼 사이즈별 css
+const Sizes = {
+  sm: css`
+    height: 28px;
+    width: 56px;
+    border-radius: 26px;
+    font-size: 12px;
+    font-weight: 400;
+  `,
+  ms: css`
+    height: 32px;
+    width: 90px;
+    border-radius: 32px;
+    font-size: 20px;
+    font-weight: 500;
+  `,
+  md: css`
+    height: 34px;
+    width: 120px;
+    border-radius: 30px;
+    font-size: 14px;
+    font-weight: 500;
+  `,
+  lg: css`
+    height: 44px;
+    width: 322px;
+    border-radius: 44px;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 18px;
+  `,
+};
+
+function Button({ disabled, children, size }) {
+  const sizeStyle = Sizes[size];
+
+  return (
+    <StyledButton disabled={disabled} sizeStyle={sizeStyle}>
+      {children}
+    </StyledButton>
+  );
+}
+
+// 버튼 디폴트값
+const StyledButton = styled.button`
+  ${(param) => param.sizeStyle}
+
+  border: none;
+  cursor: pointer;
+  background: #f26e22;
+  color: #ffffff;
+
+  &:active,
+  &:hover,
+  &:focus {
+    opacity: 0.5;
+  }
+
+  &:disabled {
+    cursor: default;
+    opacity: 0.5;
+  }
+`;
+
+export default Button;
