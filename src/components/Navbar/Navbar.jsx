6bcaf8a1a1e058055baecd2706930b51ebbd7b56
sooyyoung <sooyyyoung@gmail.com>
2022-07-05 15:36:50 +0900

    Feat: 추가 top-nav 컴포넌트

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Button from "../buttons/button";
+import "./navbar.css";
+
+export const BasicNav = () => {
+  return (
+    <div className="nav">
+      <button className="arrowButton">
+        <span className="a11yHidden">뒤로가기</span>
+      </button>
+      <button className="moreButton">
+        <span className="a11yHidden">더보기</span>
+      </button>
+    </div>
+  );
+};
+
+export const SearchNav = () => {
+  return (
+    <div className="nav">
+      <button className="arrowButton">
+        <span className="a11yHidden">뒤로가기</span>
+      </button>
+      <input className="searchInput" type="text" value="계정 검색"/>
+    </div>
+  );
+}; 
+
+export const MainNav = (props) => {
+  return (
+    <div className="nav">
+      <p className="mainTitle">{props.title}</p>
+      <button className="searchButton">
+        <span className="a11yHidden">검색하기</span>
+      </button>
+    </div>
+  );
+}; 
+
+export const UploadNav = () => {
+  return (
+    <div className="nav">
+      <button className="arrowButton">
+        <span className="a11yHidden">뒤로가기</span>
+      </button>
+      <Button className="button ms disabled uploadButton">저장</Button>
+    </div>
+  );
+};
+
+export const ChatNav = () => {
+  return (
+    <div className="nav">
+      <button className="arrowButton">
+        <span className="a11yHidden">뒤로가기</span>
+      </button>
+      <p className="chatTitle">애월읍 위니브 감귤농장</p>
+      <button className="moreButton">
+        <span className="a11yHidden">더보기</span>
+      </button>
+    </div>
+  );
+};
