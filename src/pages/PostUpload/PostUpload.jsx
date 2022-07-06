777a3e12dcde6befcdc5deb585cf051e790e7518
Miin-nii <alsdl3344@gmail.com>
2022-07-07 00:06:39 +0900

    Design: 추가 상단바

--- src/pages/PostUpload/PostUpload.jsx
+++ src/pages/PostUpload/PostUpload.jsx
@@ -1,11 +1,13 @@
 import "./postUpload.css"
 import {UploadNav} from "../../components/Navbar/Navbar"
+import Topbar from "../../components/Topbar/Topbar";
 
 export default function PostUpload(props) {
   const { userImg } = props;
 
   return (
     <div className="postUploadWrap">
+      <Topbar/>
       <UploadNav title="업로드" />
       <section className="postUploadContent">
         <img className="postUploadUserImg" src={userImg} alt="프로필 이미지" /> 
