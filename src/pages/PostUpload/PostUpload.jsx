abb5243dc84cbb89476904595608bb4be1150bdc
Miin-nii <alsdl3344@gmail.com>
2022-07-07 01:28:05 +0900

    Docs: 수정 form 태그 추가와 업로드버튼 태그 수정

--- src/pages/PostUpload/PostUpload.jsx
+++ src/pages/PostUpload/PostUpload.jsx
@@ -9,11 +9,12 @@ export default function PostUpload(props) {
     <div className="postUploadWrap">
       <Topbar/>
       <UploadNav title="업로드" />
-      <section className="postUploadContent">
+      <form className="postUploadContent" method="post">
         <img className="postUploadUserImg" src={userImg} alt="프로필 이미지" /> 
         <textarea className="postUploadTxt" cols="30" rows="10" placeholder="게시글 입력하기..."></textarea>
-      </section>
-      <button className="postUploadBtn" type="file"></button>
+        <label for="uploadInput" className="postUploadBtn"></label>
+        <input className="postUploadInput" id="uploadInput" type="file"></input>
+      </form>
     </div>
   )
 }
