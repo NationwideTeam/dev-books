10920d5f278d22075ade4cd80b7044d7d4761c3f
Miin-nii <alsdl3344@gmail.com>
2022-07-06 22:46:59 +0900

    Feat: 추가 post 업로드하는 페이지

---
+++ src/pages/PostUpload/PostUpload.jsx
@@ -0,0 +1,17 @@
+import "./postUpload.css"
+import {UploadNav} from "../../components/Navbar/Navbar"
+
+export default function PostUpload(props) {
+  const { userImg } = props;
+
+  return (
+    <div className="postUploadWrap">
+      <UploadNav title="업로드" />
+      <section className="postUploadContent">
+        <img className="postUploadUserImg" src={userImg} alt="프로필 이미지" /> 
+        <textarea className="postUploadTxt" cols="30" rows="10" placeholder="게시글 입력하기..."></textarea>
+      </section>
+      <button className="postUploadBtn" type="file"></button>
+    </div>
+  )
+}
