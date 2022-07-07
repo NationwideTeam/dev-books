26d7dd9eae6f61b32ca75b9b6f1f002ca931db2e
Jason Park <stiger3820@gmail.com>
2022-07-07 12:04:33 +0900

    Feat: 추가 Album컴포넌트 추가

---
+++ src/components/Album/Album.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./album.css";
+import Postbar from "../Postbar/Postbar";
+import album1 from "../../assets/album1.png";
+import album2 from "../../assets/album2.png";
+import album3 from "../../assets/album3.png";
+
+function Album() {
+  return (
+    <div className="album">
+      <Postbar />
+      <div className="postWrapper">
+        <img src={album1} alt="" />
+        <img src={album2} alt="" />
+        <img src={album3} alt="" />
+        <img src={album3} alt="" />
+        <img src={album1} alt="" />
+        <img src={album2} alt="" />
+        <img src={album3} alt="" />
+        <img src={album2} alt="" />
+        <img src={album1} alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Album;
--- src/pages/YourProfile/YourProfile.jsx
+++ src/pages/YourProfile/YourProfile.jsx
@@ -6,6 +6,7 @@ import ProfileInfo from "../../components/ProfileInfo/ProfileInfo";
 import Products from "../../components/Products/Products";
 import Postbar from "../../components/Postbar/Postbar";
 import Content from "../../components/Content/Content";
+import Album from "../../components/Album/Album";
 import TabMenu from "../../components/TabMenu/TabMenu";
 import Button from "../../components/Buttons/Button";
 import basicProfile from "../../assets/basic-profile.svg";
@@ -35,18 +36,8 @@ function MyProfile() {
         </div>
       </div>
       {/* <Products /> */}
+      <Album />
 
-      <Postbar />
-      <Content
-        userImg={basicProfile}
-        userName="애월읍 위니브 감귤농장"
-        userId="@ weniv_Mandarin"
-        posttext="옷을 인생을 그러므로 없으면 것은 이상은 것은 우리의 위하여, 뿐이다. 이상의 청춘의 뼈 따뜻한 그들의 그와 약동하다. 대고, 못할 넣는 풍부하게 뛰노는 인생의 힘있다."
-        postImg={postImg}
-        heartNum="58"
-        commentNum="12"
-        postDate="2020년 10월 21일"
-      />
       <div className="tabmenu">
         <TabMenu />
       </div>
