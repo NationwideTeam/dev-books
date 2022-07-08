893d0665529c5e9062e45206d6a7d58f109d6d12
Miin-nii <alsdl3344@gmail.com>
2022-07-08 09:26:19 +0900

    Docs: 수정 label for속성 htmlFor로 수정

--- src/components/ProfileImgUpload/ProfileImgUpload.jsx
+++ src/components/ProfileImgUpload/ProfileImgUpload.jsx
@@ -4,7 +4,7 @@ import './profileImgUpload.css';
 export const ProfileImgUpload = () => {
   return (
     <div>
-      <label for="profileImg" className="profileImgUpload">
+      <label htmlFor="profileImg" className="profileImgUpload">
         <img src={ProfileImg} alt="" />
       </label>
       <input type="file" id="profileImg" name="profileImg" accept="image/*" className="a11yHidden" />
