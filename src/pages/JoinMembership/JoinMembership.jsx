8a99685fdb4840d1fdc0585930c93324b688623b
Jinnie-kim <hjk7639@gmail.com>
2022-07-08 10:13:48 +0900

    Feat: 수정 label for, input id 속성 추가

--- src/pages/JoinMembership/JoinMembership.jsx
+++ src/pages/JoinMembership/JoinMembership.jsx
@@ -17,12 +17,16 @@ function JoinMembership() {
             label="이메일"
             placeholder="이메일을 입력해주세요."
             type="email"
+            for={'email'}
+            id={'email'}
           />
         </div>
         <TextInput
           label="비밀번호"
           placeholder="비밀번호를 입력해주세요."
           type="password"
+          for={'password'}
+          id={'password'}
         />
         <div
           className="loginButton"
--- src/pages/JoinProfile/JoinProfile.jsx
+++ src/pages/JoinProfile/JoinProfile.jsx
@@ -18,6 +18,8 @@ export const JoinProfile = () => {
           label="사용자 이름"
           type="text"
           placeholder="2~10자 이내여야 합니다."
+          for={'userName'}
+          id={'userName'}
         />
       </div>
       <div className="textInput">
@@ -25,6 +27,8 @@ export const JoinProfile = () => {
           label="계정 ID"
           type="text"
           placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다."
+          for={'userId'}
+          id={'userId'}
         />
       </div>
       <div className="textInput">
@@ -32,6 +36,8 @@ export const JoinProfile = () => {
           label="소개"
           type="text"
           placeholder="자신과 판매할 상품에 대해 소개해 주세요!"
+          for={'introduce'}
+          id={'introduce'}
         />
       </div>
       <div
