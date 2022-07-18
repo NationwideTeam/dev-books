21b69e08b16d26f045c89ec22e38874bb836b32f
Miin-nii <alsdl3344@gmail.com>
2022-07-18 15:56:08 +0900

    Feat: 추가 읽지않은 메시지 표시 및 채팅방이동

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,54 +1,40 @@
-import React from 'react';
-import './user.css';
+import React from "react";
+import "./user.css";
 
 export const UserSearch = (props) => {
-    return (
-        <li className='userSearchList'>
-            <img src={props.picture} alt="유저 프로필 이미지" />
-            <div className='userInfo'>
-                <strong className='userName'>
-                    {props.name}
-                </strong>
-                <strong className='userId'>
-                    {props.id}
-                </strong>
-            </div>
-        </li>
-    )
-}
+  return (
+    <li className="userSearchList">
+      <img src={props.picture} alt="유저 프로필 이미지" />
+      <div className="userInfo">
+        <strong className="userName">{props.name}</strong>
+        <strong className="userId">{props.id}</strong>
+      </div>
+    </li>
+  );
+};
 
 export const UserFollow = (props) => {
-    return (
-        <li className='userSearchList'>
-            <img src={props.picture} alt="유저 프로필 이미지" />
-            <div className='userInfo'>
-                <strong className='userName'>
-                    {props.name}
-                </strong>
-                <strong className='userId'>
-                    {props.id}
-                </strong>
-            </div>
-            <button className='userFollowButton'>{props.follow}</button>
-        </li>
-    )
-}
+  return (
+    <li className="userSearchList">
+      <img src={props.picture} alt="유저 프로필 이미지" />
+      <div className="userInfo">
+        <strong className="userName">{props.name}</strong>
+        <strong className="userId">{props.id}</strong>
+      </div>
+      <button className="userFollowButton">{props.follow}</button>
+    </li>
+  );
+};
 
 export const UserChatList = (props) => {
-  return(
-    <li className='userChatList'>
-      <img src={props.picture} alt='유저 프로필 이미지' />
-      <div className='userChatInfo'>
-        <strong className='userChatName'>
-          {props.name}
-        </strong>
-        <p className='userChatContent'>
-          {props.chat}
-        </p>
+  return (
+    <li className="userChatList" onClick={props.onClick}>
+      <img src={props.picture} alt="유저 프로필 이미지" />
+      <div className="userChatInfo">
+        <strong className="userChatName">{props.name}</strong>
+        <p className="userChatContent">{props.chat}</p>
       </div>
-      <span className='userChatDate'>
-        2020.10.25
-      </span>
+      <span className="userChatDate">2020.10.25</span>
     </li>
-  )
-}
+  );
+};
