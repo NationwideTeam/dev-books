ee6297de14db83c17667bad023c6c9d9fe37cf8a
Miin-nii <alsdl3344@gmail.com>
2022-07-19 15:15:33 +0900

    Feat: 추가 하단탭메뉴 활성화

--- src/components/TabMenu/TabMenu.jsx
+++ src/components/TabMenu/TabMenu.jsx
@@ -1,27 +1,71 @@
 import "./tabMenu.css";
-import homeButton from "../../assets/icon/icon-home.svg"
-import chatButton from "../../assets/icon/icon-message-circle.svg"
-import editButton from "../../assets/icon/icon-edit.svg"
-import profileButton from "../../assets/icon/icon-user.svg"
+import homeButton from "../../assets/icon/icon-home.svg";
+import homeButtonFill from "../../assets/icon/icon-home-fill.svg";
+import chatButton from "../../assets/icon/icon-message-circle.svg";
+import chatButtonFill from "../../assets/icon/icon-message-circle-fill.svg";
+import editButton from "../../assets/icon/icon-edit.svg";
+import profileButton from "../../assets/icon/icon-user.svg";
+import profileButtonFill from "../../assets/icon/icon-user-fill.svg";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function TabMenu() {
+  const [activate, setActivate] = useState(1);
+
   return (
     <>
       <ul className="tabMenuWrapper">
         <li className="tabMenuHome">
-          <Link to="/" className="tabLink"><img src={homeButton} alt="" />홈</Link>
+          <Link
+            to="/HomeFeed"
+            className="tabLink"
+            onClick={() => {
+              setActivate(1);
+            }}
+          >
+            <img src={activate === 1 ? homeButtonFill : homeButton} alt="" />홈
+          </Link>
         </li>
         <li className="tabMenuChat">
-          <Link to="/" className="tabLink"><img src={chatButton} alt="" />채팅</Link>
+          <Link
+            to="/chatFeed"
+            className="tabLink"
+            onClick={() => {
+              setActivate(2);
+            }}
+          >
+            <img src={activate === 2 ? chatButtonFill : chatButton} alt="" />
+            채팅
+          </Link>
         </li>
         <li className="tabMenuEdit">
-          <Link to="/" className="tabLink"><img src={editButton} alt="" />게시물 작성</Link>
+          <Link
+            to="/PostUpload"
+            className="tabLink"
+            onClick={() => {
+              setActivate(3);
+            }}
+          >
+            <img src={editButton} alt="" />
+            게시물 작성
+          </Link>
         </li>
         <li className="tabMenuProfile">
-          <Link to="/" className="tabLink"><img src={profileButton} alt="" />프로필</Link>
+          <Link
+            to="/myProfile"
+            className="tabLink"
+            onClick={() => {
+              setActivate(4);
+            }}
+          >
+            <img
+              src={activate === 4 ? profileButtonFill : profileButton}
+              alt=""
+            />
+            프로필
+          </Link>
         </li>
       </ul>
     </>
-  )
+  );
 }
