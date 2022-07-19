812666aecbd8176621c020adf8873ab3e4ca46f4
Jason Park <stiger3820@gmail.com>
2022-07-19 11:26:36 +0900

    Feat:추가 Splash 기능

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,45 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./login.css";
 import logoImage from "../../assets/logo.svg";
 import logoNameKorean from "../../assets/logoNameKorean.svg";
 import logoNameEnglish from "../../assets/logoNameEnglish.svg";
 import { useNavigate } from "react-router-dom";
 import Topbar from "../../components/Topbar/Topbar";
+import Splash from "../Splash/Splash";
 
 function LoginPage() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   let navigate = useNavigate();
   return (
     <>
-      <article className="loginMobileScreen">
-        <Topbar />
-        <div className="loginLogo">
-          <img className="loginLogoImage" src={logoImage} alt="데브북스" />
-          <img src={logoNameKorean} alt="데브북스" />
-          <img src={logoNameEnglish} alt="데브북스" />
-        </div>
-        <article className="loginLink">
-          <div className="loginKakao">카카오톡 계정으로 로그인</div>
-          <div className="loginGoogle">구글 계정으로 로그인</div>
-          <div className="loginFacebook">페이스북 계정으로 로그인</div>
-          <div className="loginWrapper">
-            <p
-              onClick={() => {
-                navigate("/loginEmail");
-              }}
-            >
-              이메일로 로그인
-            </p>
-            <div id="divisionLine"></div>
-            <p
-              onClick={() => {
-                navigate("/joinMembership");
-              }}
-            >
-              회원가입
-            </p>
+      {loading ? (
+        //로딩 시 스플레쉬 페이지
+        <Splash />
+      ) : (
+        //로딩 후 로그인 페이지
+        <article className="loginMobileScreen">
+          <Topbar />
+          <div className="loginLogo">
+            <img className="loginLogoImage2" src={logoImage} alt="데브북스" />
+            <img src={logoNameKorean} alt="데브북스" />
+            <img src={logoNameEnglish} alt="데브북스" />
           </div>
+          <article className="loginLink">
+            <div className="loginKakao">카카오톡 계정으로 로그인</div>
+            <div className="loginGoogle">구글 계정으로 로그인</div>
+            <div className="loginFacebook">페이스북 계정으로 로그인</div>
+            <div className="loginWrapper">
+              <p
+                onClick={() => {
+                  navigate("/loginEmail");
+                }}
+              >
+                이메일로 로그인
+              </p>
+              <div id="divisionLine"></div>
+              <p
+                onClick={() => {
+                  navigate("/joinMembership");
+                }}
+              >
+                회원가입
+              </p>
+            </div>
+          </article>
         </article>
-      </article>
+      )}
     </>
   );
 }
