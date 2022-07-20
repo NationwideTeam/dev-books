e1c4d416b7edd5a4d76fb66404357f77df0f3ae5
Miin-nii <alsdl3344@gmail.com>
2022-07-20 19:47:34 +0900

    Feat: 추가 상품삭제기능

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,20 +1,61 @@
 import React, { useState, useEffect } from "react";
 import "./products.css";
 import { Link } from "react-router-dom";
+import Modal from "../Modal/Modal";
+import ModalContent from "../ModalContent/ModalContent";
+import Alert from "../Alert/Alert";
 
 function Product(props) {
-  const { text, price, image } = props;
+  const { value, text, price, image, onClick } = props;
+
+  const [prodModal, setProdModal] = useState(false);
+  const [prodAlert, setProdAlert] = useState(false);
+
   return (
-    // !!링크 추후 변경!!
-    <Link to="/myProfile" className="product">
-      <img src={image} alt="" />
-      <p className="productText">{text}</p>
-      <p className="productPrice">{price} 원</p>
-    </Link>
+    <>
+      {/* // !!링크 추후 변경!! */}
+      <Link
+        to="/myProfile"
+        className="product"
+        onClick={() => setProdModal(true)}
+      >
+        <img src={image} alt="" />
+        <p className="productText">{text}</p>
+        <p className="productPrice">{price} 원</p>
+      </Link>
+      <div
+        className={prodModal ? "prodModal" : "disabledProdPopup"}
+        onClick={() => {
+          setProdModal(false);
+        }}
+      >
+        <Modal>
+          <ModalContent
+            txt="삭제"
+            onClick={() => {
+              setProdAlert(true);
+              setProdModal(false);
+            }}
+          />
+          <ModalContent txt="수정" />
+          <ModalContent txt="웹사이트에서 상품 보기" />
+        </Modal>
+      </div>
+      <div className={prodAlert ? "prodModal" : "disabledProdPopup"}>
+        <Alert
+          message="상품을 삭제할까요?"
+          cancel="취소"
+          confirm="삭제"
+          value={value}
+          onClickConfirm={onClick}
+          onClickCancel={() => setProdAlert(false)}
+        />
+      </div>
+    </>
   );
 }
 
-function Products() {
+function Products(props) {
   const [products, setProducts] = useState([]);
   useEffect(function () {
     async function getUserProduct() {
@@ -39,6 +80,7 @@ function Products() {
         const resUserProductJson = await resUserProduct.json();
         const userProduct = resUserProductJson.product;
         setProducts(userProduct);
+        console.log(userProduct);
       } catch (err) {
         console.error("err", err);
       }
@@ -54,9 +96,11 @@ function Products() {
           return (
             <Product
               key={item.id}
+              value={item.id}
               image={item.itemImage}
               text={item.itemName}
               price={item.price}
+              onClick={props.onClick}
             />
           );
         })}
