e98b6a195888d4e3675badff0a5e5a71ab3e8ab6
sooyyoung <sooyyyoung@gmail.com>
2022-07-21 09:50:48 +0900

    Feat: 추가 AccountName 체크 로직 / ProfileInfo, Content 컴포넌트에 accountName값 props로 전달

--- src/pages/MyProfile/MyProfile.jsx
+++ src/pages/MyProfile/MyProfile.jsx
@@ -10,7 +10,7 @@ import TabMenu from "../../components/TabMenu/TabMenu";
 import Button from "../../components/Buttons/Button";
 import basicProfile from "../../assets/basic-profile.svg";
 import postImg from "../../assets/post-img-example.png";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import Modal from "../../components/Modal/Modal";
 import ModalContent from "../../components/ModalContent/ModalContent";
 
@@ -18,6 +18,17 @@ function MyProfile() {
   const [navModal, setNavModal] = useState(false);
 
   let navigate = useNavigate();
+  const location = useLocation();
+
+  // accountName 체크 (사용자 프로필 or 마이 프로필 확인)
+  const checkAccountName = () => {
+    if (location.pathname.split("/")[1] === "myProfile") {
+      const accountName = localStorage.getItem("accountname");
+      return accountName;
+    }
+  };
+
+  const accountName = checkAccountName();
 
   const prodDelete = async (e) => {
     const url = "https://mandarin.api.weniv.co.kr";
@@ -51,13 +62,7 @@ function MyProfile() {
           setNavModal(true);
         }}
       />
-      <ProfileInfo
-        follower="2950"
-        followings="128"
-        name="애월읍 위니브 감귤농장"
-        id="@ weniv_Mandarin"
-        description="애월읍 감귤 전국 배송, 귤따기 체험, 감귤 농장"
-      />
+      <ProfileInfo accountName={accountName} />
       <div className="MyProfileButtonWrapper">
         <Button className="button md active">프로필 수정</Button>
         <Button
@@ -72,17 +77,7 @@ function MyProfile() {
       <Products onClick={prodDelete} />
 
       <Postbar />
-      <Content
-        userImg={basicProfile}
-        userName="애월읍 위니브 감귤농장"
-        userId="@ weniv_Mandarin"
-        posttext="옷을 인생을 그러므로 없으면 것은 이상은 것은 우리의 위하여, 뿐이다. 이상의 청춘의 뼈 따뜻한 그들의 그와 약동하다. 대고, 못할 넣는 풍부하게 뛰노는 인생의 힘있다."
-        postImg={postImg}
-        heartNum="58"
-        commentNum="12"
-        postDate="2020년 10월 21일"
-      />
-
+      <Content accountName={accountName} />
       <div className="blank"></div>
       <div
         className={navModal ? "myProfileNavModal" : "disabledMyProfilePopup"}
