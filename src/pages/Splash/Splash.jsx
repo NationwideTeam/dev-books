e6bf4ce97ba5748c82fb9123320e8eedbeffa213
sooyyoung <sooyyyoung@gmail.com>
2022-07-22 10:21:20 +0900

    Design: 수정 Splash, Login 페이지 배경컬러, 로고 위치

--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import logoImage from "../../assets/logo.svg";
 import logoNameKorean from "../../assets/logoNameKorean.svg";
-import logoNameEnglish from "../../assets/logoNameEnglish.svg";
 import Topbar from "../../components/Topbar/Topbar";
 import "./splash.css";
 
@@ -12,8 +11,7 @@ function Splash() {
         <Topbar />
         <div className="loginLogo">
           <img className="loginLogoImage" src={logoImage} alt="데브북스" />
-          <img src={logoNameKorean} alt="데브북스" />
-          <img src={logoNameEnglish} alt="데브북스" />
+          <img src={logoNameKorean} className="logoKo" alt="데브북스" />
         </div>
       </article>
     </>
