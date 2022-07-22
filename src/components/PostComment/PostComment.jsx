c6c8b8c165118fa523d2862083e6c54f5ced80df
Jinnie-kim <hjk7639@gmail.com>
2022-07-22 10:28:50 +0900

    Feat: 추가 댓글 불러오기 기능

--- src/components/PostComment/PostComment.jsx
+++ src/components/PostComment/PostComment.jsx
@@ -1,20 +1,28 @@
-import "./postComment.css";
+// PostComment.jsx
+import './postComment.css';
 
 export default function PostComment(props) {
-
-  const { commentUserImg, commentUserName, commentTimeNum, commenttext } = props;
-
-  return (
-    <li className="postComment">
-      <div className="commentUser">
-        <img className="commentUserImg" src={commentUserImg} alt="유저 기본 프로필 이미지" />
-        <strong className="commentUserName">  
-            {commentUserName}
-        </strong>
-        <span className="commentTime">{commentTimeNum}</span>
-        <button className="moreCommentBtn" type="button"></button>
-      </div>
-      <p className="commenttxt">{commenttext}</p>
-    </li>
-  )
+  return props.commentInfo.map((file, index) => {
+    return (
+      <li className="postComment" key={index}>
+        <div className="commentUser">
+          <img
+            className="commentUserImg"
+            src={file.author.image}
+            alt="유저 기본 프로필 이미지"
+          />
+          <strong className="commentUserName">{file.author.username}</strong>
+          <span className="commentTime">
+            {file.createdAt
+              .slice(0, 11)
+              .replace('-', '년')
+              .replace('-', '월')
+              .replace('T', '일')}
+          </span>
+          <button className="moreCommentBtn" type="button"></button>
+        </div>
+        <p className="commenttxt">{file.content}</p>
+      </li>
+    );
+  });
 }
