c226b96d79c0a540c72b42c08f00f1634f7415c3
sooyyoung <sooyyyoung@gmail.com>
2022-07-26 12:49:10 +0900

    Docs: 수정 코드 정리

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import "./user.css";
 import Button from "../Buttons/Button";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export const UserSearch = (props) => {
   return (
