eed094fdda9c4227c99f7c367269a89974d3c473
miin-nii <alsdl3344@gmail.com>
2022-09-02 16:49:59 +0900

    Docs: 수정 ChatRoom 페이지 styled-components 적용

--- src/pages/ChatRoom/ChatRoom.jsx
+++ src/pages/ChatRoom/ChatRoom.jsx
@@ -1,4 +1,3 @@
-import "./chatRoom.css";
 import Topbar from "../../components/Topbar/Topbar";
 import { ChatNav } from "../../components/Navbar/Navbar";
 import userProfile from "../../assets/basic-profile-img.svg";
@@ -6,6 +5,22 @@ import { useState } from "react";
 import Modal from "../../components/Modal/Modal";
 import ModalContent from "../../components/ModalContent/ModalContent";
 import { useNavigate } from "react-router-dom";
+import {
+  ChatRoomWrap,
+  ChatMain,
+  ChatList,
+  ChatTime,
+  UserChatImg,
+  UserChatMsg,
+  MyChatMsg,
+  ChatInputWrap,
+  ChatInputForm,
+  ChatUploadBtn,
+  ChatUploadInput,
+  ChatMsgInput,
+  ChatSubmit,
+  ChatModal,
+} from "./chatRoom.style";
 
 export default function ChatRoom() {
   const [chatInp, setChatInp] = useState("");
@@ -18,7 +33,7 @@ export default function ChatRoom() {
   };
 
   return (
-    <div className="chatRoomWrap">
+    <ChatRoomWrap>
       <Topbar />
       <ChatNav
         title="애월읍 위니브 감귤농장"
@@ -26,69 +41,55 @@ export default function ChatRoom() {
           setModal(true);
         }}
       />
-      <section className="chatMain">
+      <ChatMain>
         <ul>
-          <li className="userChat">
-            <img className="userChatImg" src={userProfile} alt="" />
-            <div className="userMessage">
+          <ChatList>
+            <UserChatImg src={userProfile} />
+            <UserChatMsg>
               옷을 인생을 그러므로 없으면 것은 이상은 것은 우리의 위하여,
               뿐이다. 이상의 청춘의 뼈 따뜻한 그들의 그와 약동하다. 대고, 못할
               넣는 풍부하게 뛰노는 인생의 힘있다.
-            </div>
-            <div className="userChatTime">12:39</div>
-          </li>
-          <li className="userChat">
-            <img className="userChatImg" src={userProfile} alt="" />
-            <div className="userMessage">
-              안녕하세요. 감귤 사고싶어요요요요요
-            </div>
-            <div className="userChatTime">12:41</div>
-          </li>
-          <li className="myChat">
-            <div className="myChatTime">12:50</div>
-            <div className="myMessage">네 말씀하세요.</div>
-          </li>
-          <li className="myChat">
-            <div className="myChatTime">12:51</div>
-            <div className="myMessage myMessageImg"></div>
-          </li>
+            </UserChatMsg>
+            <ChatTime>12:39</ChatTime>
+          </ChatList>
+          <ChatList>
+            <UserChatImg src={userProfile} />
+            <UserChatMsg>안녕하세요. 감귤 사고싶어요요요요요</UserChatMsg>
+            <ChatTime>12:41</ChatTime>
+          </ChatList>
+          <ChatList myChat>
+            <ChatTime>12:50</ChatTime>
+            <MyChatMsg>네 말씀하세요.</MyChatMsg>
+          </ChatList>
+          <ChatList myChat>
+            <ChatTime>12:51</ChatTime>
+            <MyChatMsg chatImg></MyChatMsg>
+          </ChatList>
         </ul>
-      </section>
-      <section className="chatInputWrap">
-        <form className="chatInputForm">
-          <label htmlFor="chatUploadFile" className="chatUploadBtn"></label>
-          <input
-            className="chatUpload"
+      </ChatMain>
+      <ChatInputWrap>
+        <ChatInputForm>
+          <ChatUploadBtn htmlFor="chatUploadFile"></ChatUploadBtn>
+          <ChatUploadInput
             id="chatUploadFile"
-            type="file"
             onChange={(e) => setChatImg(e.target.files[0].name)}
-          ></input>
+          ></ChatUploadInput>
           <label htmlFor="chatRoomInput" className="a11yHidden">
             메시지 입력창 입니다.
           </label>
-          <input
-            type="text"
-            placeholder="메시지 입력하기..."
-            className="chatInput"
+          <ChatMsgInput
             id="chatRoomInput"
             onChange={(e) => {
               setChatInp(e.target.value);
             }}
           />
-          <button
-            className={
-              chatImg === "" && chatInp === ""
-                ? "chatSubmit submitDisabled"
-                : "chatSubmit"
-            }
-            type="button"
-          >
+          <ChatSubmit className={chatImg === "" && chatInp === ""}>
             전송
-          </button>
-        </form>
-      </section>
-      <div
-        className={modal ? "chatModal" : "disabledChatModal"}
+          </ChatSubmit>
+        </ChatInputForm>
+      </ChatInputWrap>
+      <ChatModal
+        className={modal}
         onClick={() => {
           setModal(false);
         }}
@@ -96,7 +97,7 @@ export default function ChatRoom() {
         <Modal>
           <ModalContent txt="채팅방 나가기" onClick={next} />
         </Modal>
-      </div>
-    </div>
+      </ChatModal>
+    </ChatRoomWrap>
   );
 }
