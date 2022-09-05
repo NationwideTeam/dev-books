866eb0eb63f3765db003ff07960ee4674fa29ce0
sooyyoung <sooyyyoung@gmail.com>
2022-09-05 20:54:18 +0900

    Docs: 수정 JoinProfile 페이지 styled-components 적용

---
+++ src/pages/JoinProfile/joinProfile.style.js
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const JoinProfileWrap = styled.section`
+  width: 390px;
+  height: 844px;
+  margin: 0 auto;
+  background-color: #fff;
+`;
+
+export const JoinProfileTitle = styled.h1`
+  padding-top: 30px;
+  font-size: 24px;
+  font-weight: 500;
+  text-align: center;
+  color: #000000;
+`;
+
+export const JoinProfileText = styled.p`
+  margin-top: 12px;
+  font-size: 14px;
+  font-weight: 400;
+  text-align: center;
+  color: #767676;
+`;
+
+export const TextInputWrap = styled.div`
+  width: 322px;
+  margin: 0 auto;
+  margin-bottom: 16px;
+`;
+
+export const JoinButtonWrap = styled.div`
+  width: 322px;
+  margin: 30px auto;
+  display: block;
+`;
+
+export const ErrorMessage = styled.span`
+  font-size: 12px;
+  font-weight: 400;
+  color: #eb5757;
+  span {
+    text-align: center;
+    margin: 0 auto;
+    margin-bottom: 10px;
+    display: block;
+  }
+`;
